e7e1286962faa5f1712d4e4998af4c5482e299c7
Eric S. Bullington <eric.s.bullington@gmail.com>
2014-10-25 11:01:31 -0400

    Expanded message in js template and added strict mode

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,5 +1,6 @@
 (function() {
+  'use strict';
 
-  console.log('testing');
+  console.log('Hello from flask-bootstrap.  See app/static/js/main.js for a js file that is automatically compiled and minified into a versioned app/static/gen/bundle.js file');
 
 }).call(this);
